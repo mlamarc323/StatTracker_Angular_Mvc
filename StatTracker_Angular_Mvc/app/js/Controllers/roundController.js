6d5ef7ae2eceb58c1a4412d440324c812f28d586
mlamarca@practicevelocity.com <mlamarca@practicevelocity.com>
2014-10-17 15:17:35 -0500

    Added Round page summary.

--- StatTracker_Angular_Mvc/app/js/Controllers/roundController.js
+++ StatTracker_Angular_Mvc/app/js/Controllers/roundController.js
@@ -1,21 +1,38 @@
-﻿'use strict';
+﻿
+'use strict';
 
 statTrackerApp.controller('roundController',
-    function($scope, $http, $window, $route) {
+    function ($scope, $http, $window, $route) {
         $scope.newRound = {};
-        $scope.roundDetails = {};
+        $scope.savedRound = {};
+        $scope.roundDetails = [];
+        $scope.rounds = [];
 
         $scope.facilities = {};
         $scope.courses = {};
         $scope.tees = {};
+        $scope.holes = {};
+
+        //get rounds on load
+        $http.get("/api/round/")
+                .then(function (result) {
+                    // Successful
+                    angular.copy(result.data, $scope.rounds);
+
+                    //$scope.roundDetails = result.data;
+                },
+                    function () {
+                        // Error
+                        alert("Could not load rounds.");
+                    });
 
         //get facilities
         $http.get("/api/facilities")
           .then(function (result) {
               // Successful
               //angular.copy(result.data, $scope.facilities);
-                $scope.facilities = result.data;
-            },
+              $scope.facilities = result.data;
+          },
           function () {
               // Error
               //deferred.reject();
@@ -23,14 +40,14 @@ statTrackerApp.controller('roundController',
           });
 
         //get courses
-        $scope.getCourses = function(facilityId) {
+        $scope.getCourses = function (facilityId) {
             $http.get("/api/facilities/" + facilityId + "/courses")
-                .then(function(result) {
+                .then(function (result) {
                     // Successful
                     $scope.courses = result.data;
-                        //angular.copy(result.data, $scope.courses);
-                    },
-                    function() {
+                    //angular.copy(result.data, $scope.courses);
+                },
+                    function () {
                         // Error
                         //deferred.reject();
                         alert("Could not load Courses.");
@@ -52,16 +69,62 @@ statTrackerApp.controller('roundController',
                     });
         };
 
+        //get holes
+        $scope.getHoles = function (teeId) {
+            $http.get("/api/hole/" + teeId)
+                .then(function (result) {
+                    // Successful
+                    $scope.holes = result.data;
+                },
+                    function () {
+                        // Error
+                        //deferred.reject();
+                        alert("Could not load holes.");
+                    });
+        };
+
         $scope.addRound = function () {
             $http.post("/api/round/", $scope.newRound)
-                .then(function () {
+                .then(function (result) {
                     // Successful
                     //angular.copy(result.data, $scope.facilities);
                     //$window.location = "#/facilities";
+                    $scope.savedRound = result.data;
+                    $scope.getRoundDetails();
+                    $window.location.href = '#/roundDetail';
                 },
           function () {
               // Error
               alert("Could not save new round");
           });
         };
+
+        $scope.getRoundDetails = function () {
+            $http.get("/api/roundDetail/" + $scope.savedRound.roundId)
+                .then(function (result) {
+                    // Successful
+                    angular.copy(result.data, $scope.roundDetails);
+
+                    //$scope.roundDetails = result.data;
+                },
+                    function () {
+                        // Error
+                        alert("Could not load round details.");
+                    });
+        };
+
+        $scope.getRoundDetailsById = function (roundId) {
+            $http.get("/api/roundDetail/" + roundId)
+                .then(function (result) {
+                    // Successful
+                    angular.copy(result.data, $scope.roundDetails);
+
+                    //$scope.roundDetails = result.data;
+                },
+                    function () {
+                        // Error
+                        alert("Could not load round details.");
+                    });
+        };
+
     });
